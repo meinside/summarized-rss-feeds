2fc3003faf35d3bf16284445a52f74543ac67d36
Sungjin Han <meinside@meinside.dev>
2024-07-24 13:01:14 +0900

    Add files

---
+++ README.md
@@ -0,0 +1,83 @@
+# summarized-rss-feeds
+
+Fetch RSS feeds, summarize each of them with Google Gemini API, and then serve them as another RSS XML.
+
+## build
+
+```bash
+$ go build
+```
+
+## configure
+
+Create `config.json` file with content:
+
+```json
+{
+  "google_ai_api_key": "AIabcdefghijklmnopqrstuvwxyz0123456789",
+  "google_ai_model": "gemini-1.5-flash-latest",
+  "db_files_dir": "/path/to/summarized-rss-feeds/caches/",
+  "desired_language": "Korean",
+  "verbose": false,
+  //"verbose": true,
+
+  "rss_feeds": [
+    {
+      "name": "Tech RSS Feeds Summarized",
+      "cache_filename": "tech.db",
+      "serve_path": "/tech",
+      "feed_urls": [
+        "https://hnrss.org/newest?points=50",
+        "https://lobste.rs/rss",
+      ],
+      "publish_title": "Summarized RSS Feeds About Technology",
+      "publish_link": "https://no-such-domain.com",
+      "publish_description": "Tech RSS Feeds Summarized with Google Gemini 1.5 Flash",
+      "publish_author": "rss-feeds-summarizer",
+      "publish_email": "no-such-email@no-such-domain.com",
+    },
+  ],
+  "fetch_feeds_interval_minutes": 5,
+  "permitted_user_agents": [
+    "Feedly", // feedly crawler bot's user agent
+  ],
+
+  "rss_server_port": 8080,
+}
+```
+
+This application will poll new feeds from `rss_feeds[].feed_urls` with interval of `fetch_feeds_interval_minutes`.
+
+Fetched feeds' contents will be summarized in `desired_language` with your `google_ai_api_key`, and cached in `rss_feeds[].cache_filename` in `db_files_dir`.
+
+Resulting RSS feeds' RSS XML will be served on: yourserver:`rss_server_port`/`rss_feeds[].serve_path`(eg. `localhost:8080/tech`).
+
+Make the URL public and register it on your desired RSS reader, then you'll see your summarized RSS feeds in a few hours.
+
+## run
+
+```bash
+$ summarized-rss-feeds /path/to/config.json
+```
+
+### run as a service (systemd)
+
+```
+[Unit]
+Description=Summarized RSS Feeds (Server)
+After=syslog.target
+After=network.target
+
+[Service]
+Type=simple
+User=ubuntu
+Group=ubuntu
+WorkingDirectory=/dir/to/summarized-rss-feeds
+ExecStart=/dir/to/summarized-rss-feeds/summarized-rss-feeds /path/to/config.json
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+```
+
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	rf "github.com/meinside/rss-feeds-go"
+)
+
+const (
+	defaultGoogleAIModel   = "gemini-1.5-flash-latest"
+	defaultDesiredLanguage = "English"
+
+	defaultFetchFeedsIntervalMinutes = 5
+)
+
+// config struct
+type config struct {
+	// System
+	GoogleAIAPIKey   string  `json:"google_ai_api_key"`
+	GoogleAIModel    *string `json:"google_ai_model,omitempty"`
+	DBFilesDirectory string  `json:"db_files_dir"`
+	DesiredLanguage  *string `json:"desired_language,omitempty"`
+	Verbose          bool    `json:"verbose,omitempty"`
+
+	// RSS
+	RSSFeeds                  []configRSSFeed `json:"rss_feeds"`
+	FetchFeedsIntervalMinutes int             `json:"fetch_feeds_interval_minutes,omitempty"`
+	PermittedUserAgents       []string        `json:"permitted_user_agents,omitempty"`
+
+	// RSS server port
+	RSSServerPort int `json:"rss_server_port"`
+}
+
+// configRSSFeed struct
+type configRSSFeed struct {
+	Name          string   `json:"name"`
+	CacheFilename string   `json:"cache_filename"`
+	ServePath     string   `json:"serve_path"`
+	FeedURLs      []string `json:"feed_urls"`
+
+	PublishTitle       *string `json:"publish_title,omitempty"`
+	PublishLink        *string `json:"publish_link,omitempty"`
+	PublishDescription *string `json:"publish_description,omitempty"`
+	PublishAuthor      *string `json:"publish_author,omitempty"`
+	PublishEmail       *string `json:"publish_email,omitempty"`
+}
+
+// read config from given `filepath`
+func readConfig(filepath string) (conf config, err error) {
+	var bytes []byte
+	if bytes, err = os.ReadFile(filepath); err == nil {
+		if bytes, err = rf.StandardizeJSON(bytes); err == nil {
+			if err = json.Unmarshal(bytes, &conf); err == nil {
+				// set default values
+				if conf.GoogleAIModel == nil {
+					conf.GoogleAIModel = ptr(defaultGoogleAIModel)
+				}
+				if conf.DesiredLanguage == nil {
+					conf.DesiredLanguage = ptr(defaultDesiredLanguage)
+				}
+				if conf.FetchFeedsIntervalMinutes <= 0 {
+					conf.FetchFeedsIntervalMinutes = defaultFetchFeedsIntervalMinutes
+				}
+
+				return conf, nil
+			}
+		}
+	}
+
+	return conf, err
+}
+
+// return the pointer of given value `v`
+func ptr[T any](v T) *T {
+	val := v
+	return &val
+}
+
+// print help message
+func printHelp(cmd string) {
+	log.Printf(`> Usage:
+
+  * fetch, summarize, cache, and serve RSS feed items
+    %[1]s [CONFIG_FILEPATH]
+`, cmd)
+}
+
+func main() {
+	cmd := os.Args[0]
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		configFilepath := args[0]
+		if conf, err := readConfig(configFilepath); err == nil {
+			run(conf)
+
+			os.Exit(0)
+		} else {
+			log.Printf("> failed to read config: %s", err)
+		}
+	} else {
+		printHelp(cmd)
+	}
+
+	os.Exit(1)
+}
